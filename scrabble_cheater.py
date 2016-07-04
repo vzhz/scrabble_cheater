4969fa4589c5c6094457be99e5d975ed14571ab1
vzhz <vehanus@gmail.com>
2016-07-04 19:23:27 -0400

    added fcn to add points for each word

--- scrabble_cheater.py
+++ scrabble_cheater.py
@@ -13,9 +13,9 @@ for i in f:
     valid_words.append(i.lower())
 
 rack = sys.argv[1].lower()
-if len(rack) !=7 or rack.isalpha() == False: #the "only letters" test doesn't work #also blanks
-print "Please rerun command and seven-letter rack to play."
-exit()
+if len(rack) != 7 or rack.isalpha() is False: #the "only letters" test doesn't work #also blanks
+    print("Please rerun command and seven-letter rack to play.")
+    exit()
 
 winning_words = []
 for word in valid_words:
@@ -23,22 +23,24 @@ for word in valid_words:
     for letter in word:
         if letter in rack_temp:
             rack_temp.remove(letter)
-            if len(rack)-len(rack_temp)==len(word):
+            if len(rack) - len(rack_temp) == len(word):
                 winning_words.append(word)
-                print winning_words
 
-just_points_winning_words = []
-for word in winning_words:
+def add_points_word(word):
     points_word = 0
     for letter in word:
         if letter in scores: #scores letters are strings might need to split the words in valid words into their individual letters
-     points_word += scores[letter]
-     just_points_winning_words.append(points_word)
-print just_points_winning_words
+            points_word += scores[letter]
+    return points_word
+
+just_points_winning_words = []
+for word in winning_words:
+    points_word = add_points_word(word)
+    just_points_winning_words.append(points_word)
 
 winning_words_and_points = dict(zip(winning_words, just_points_winning_words))
 
-    #also recommend I look into list comprehensions
-
 #sorting dict by value so the words that give you the most points are listed first
-print "For the maximum points, spell this word: " + max(winning_words_and_points, key=winning_words_and_points.get)
+print("")
+print("For the maximum points, spell this word: " + max(winning_words_and_points, key=winning_words_and_points.get))
+print("")
